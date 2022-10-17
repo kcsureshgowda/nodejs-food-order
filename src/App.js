bd9d2eb0f14382538ace7406957e52a97b1b2b5a
Suresh K C <su268302@wipro.com>
2022-10-17 15:41:58 +0530

    useState added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
+import Cart from './components/Cart/Cart';
+
 
 function App() {
-  
+    
     const [cartIsShown, setCartIsShown] = useState(false);
 
     function showCartHandler() {
@@ -17,7 +19,7 @@ function App() {
 
     return ( 
         <CartProvider>
-            {cartIsShown && <div> Cart.... </div>} 
+            {cartIsShown && <Cart onClose={ hideCartHandler}/>} 
             <Header onShowCart = { showCartHandler } />
             <main>
                 <Meals/>
@@ -26,4 +28,4 @@ function App() {
         );
     }
 
-    export default App;
+export default App;
