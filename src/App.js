33d8b8df23ef2c102094edee40f0401880660221
Suresh K C <su268302@wipro.com>
2022-10-17 15:19:26 +0530

    cart logic

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
 function App() {
+  
     const [cartIsShown, setCartIsShown] = useState(false);
 
     function showCartHandler() {
