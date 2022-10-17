554eb35e0dd5be6b001ac4e00bffe747a8e2fbf7
Suresh K C <su268302@wipro.com>
2022-10-17 14:50:10 +0530

    format issue fix

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
+
 function App() {
-  return (
+    return ( 
     <CartProvider>
-      <Header />
-      <main>
-        <Meals />
-      </main>
+        <Header/>
+        <main>
+            < Meals />
+        </main >
     </CartProvider>
-  );
+    );
 }
 
 export default App;
